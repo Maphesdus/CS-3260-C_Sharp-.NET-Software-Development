9e05d07010505b64891f4505d650c458f6d85b6e
Maphesdus <nanobot18@gmail.com>
2022-02-10 16:42:14 -0700

    Add files via upload

---
+++ Lab_03_AHA/Lab_03_AHA/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Lab_03_AHA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Lbl_RealNumbers = new System.Windows.Forms.Label();
+            this.Lbl_ImaginaryNumbers = new System.Windows.Forms.Label();
+            this.Lbl_plusSign1 = new System.Windows.Forms.Label();
+            this.Lbl_plusSign2 = new System.Windows.Forms.Label();
+            this.Lbl_plusSign3 = new System.Windows.Forms.Label();
+            this.Lbl_horizontalLine = new System.Windows.Forms.Label();
+            this.Lbl_i1 = new System.Windows.Forms.Label();
+            this.Lbl_i2 = new System.Windows.Forms.Label();
+            this.Lbl_i3 = new System.Windows.Forms.Label();
+            this.Lbl_Add = new System.Windows.Forms.Label();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.Tb_Real1 = new System.Windows.Forms.TextBox();
+            this.Tb_Real2 = new System.Windows.Forms.TextBox();
+            this.Tb_Imaginary1 = new System.Windows.Forms.TextBox();
+            this.Tb_Imaginary2 = new System.Windows.Forms.TextBox();
+            this.Tb_Real3 = new System.Windows.Forms.TextBox();
+            this.Tb_Imaginary3 = new System.Windows.Forms.TextBox();
+            this.Btn_RunTests = new System.Windows.Forms.Button();
+            this.Btn_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(194, 242);
+            this.Calculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(112, 35);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Lbl_RealNumbers
+            // 
+            this.Lbl_RealNumbers.AutoSize = true;
+            this.Lbl_RealNumbers.Location = new System.Drawing.Point(63, 58);
+            this.Lbl_RealNumbers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_RealNumbers.Name = "Lbl_RealNumbers";
+            this.Lbl_RealNumbers.Size = new System.Drawing.Size(114, 20);
+            this.Lbl_RealNumbers.TabIndex = 7;
+            this.Lbl_RealNumbers.Text = "Real Numbers:";
+            // 
+            // Lbl_ImaginaryNumbers
+            // 
+            this.Lbl_ImaginaryNumbers.AutoSize = true;
+            this.Lbl_ImaginaryNumbers.Location = new System.Drawing.Point(276, 58);
+            this.Lbl_ImaginaryNumbers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_ImaginaryNumbers.Name = "Lbl_ImaginaryNumbers";
+            this.Lbl_ImaginaryNumbers.Size = new System.Drawing.Size(150, 20);
+            this.Lbl_ImaginaryNumbers.TabIndex = 8;
+            this.Lbl_ImaginaryNumbers.Text = "Imaginary Numbers:";
+            // 
+            // Lbl_plusSign1
+            // 
+            this.Lbl_plusSign1.AutoSize = true;
+            this.Lbl_plusSign1.Location = new System.Drawing.Point(238, 88);
+            this.Lbl_plusSign1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_plusSign1.Name = "Lbl_plusSign1";
+            this.Lbl_plusSign1.Size = new System.Drawing.Size(18, 20);
+            this.Lbl_plusSign1.TabIndex = 9;
+            this.Lbl_plusSign1.Text = "+";
+            // 
+            // Lbl_plusSign2
+            // 
+            this.Lbl_plusSign2.AutoSize = true;
+            this.Lbl_plusSign2.Location = new System.Drawing.Point(238, 128);
+            this.Lbl_plusSign2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_plusSign2.Name = "Lbl_plusSign2";
+            this.Lbl_plusSign2.Size = new System.Drawing.Size(18, 20);
+            this.Lbl_plusSign2.TabIndex = 10;
+            this.Lbl_plusSign2.Text = "+";
+            // 
+            // Lbl_plusSign3
+            // 
+            this.Lbl_plusSign3.AutoSize = true;
+            this.Lbl_plusSign3.Location = new System.Drawing.Point(238, 205);
+            this.Lbl_plusSign3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_plusSign3.Name = "Lbl_plusSign3";
+            this.Lbl_plusSign3.Size = new System.Drawing.Size(18, 20);
+            this.Lbl_plusSign3.TabIndex = 11;
+            this.Lbl_plusSign3.Text = "+";
+            // 
+            // Lbl_horizontalLine
+            // 
+            this.Lbl_horizontalLine.AutoSize = true;
+            this.Lbl_horizontalLine.Location = new System.Drawing.Point(18, 162);
+            this.Lbl_horizontalLine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_horizontalLine.Name = "Lbl_horizontalLine";
+            this.Lbl_horizontalLine.Size = new System.Drawing.Size(494, 20);
+            this.Lbl_horizontalLine.TabIndex = 12;
+            this.Lbl_horizontalLine.Text = "---------------------------------------------------------------------------------" +
+    "----------------";
+            // 
+            // Lbl_i1
+            // 
+            this.Lbl_i1.AutoSize = true;
+            this.Lbl_i1.Location = new System.Drawing.Point(441, 88);
+            this.Lbl_i1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_i1.Name = "Lbl_i1";
+            this.Lbl_i1.Size = new System.Drawing.Size(12, 20);
+            this.Lbl_i1.TabIndex = 13;
+            this.Lbl_i1.Text = "i";
+            // 
+            // Lbl_i2
+            // 
+            this.Lbl_i2.AutoSize = true;
+            this.Lbl_i2.Location = new System.Drawing.Point(441, 129);
+            this.Lbl_i2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_i2.Name = "Lbl_i2";
+            this.Lbl_i2.Size = new System.Drawing.Size(12, 20);
+            this.Lbl_i2.TabIndex = 14;
+            this.Lbl_i2.Text = "i";
+            // 
+            // Lbl_i3
+            // 
+            this.Lbl_i3.AutoSize = true;
+            this.Lbl_i3.Location = new System.Drawing.Point(441, 200);
+            this.Lbl_i3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_i3.Name = "Lbl_i3";
+            this.Lbl_i3.Size = new System.Drawing.Size(12, 20);
+            this.Lbl_i3.TabIndex = 15;
+            this.Lbl_i3.Text = "i";
+            // 
+            // Lbl_Add
+            // 
+            this.Lbl_Add.AutoSize = true;
+            this.Lbl_Add.Location = new System.Drawing.Point(474, 162);
+            this.Lbl_Add.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_Add.Name = "Lbl_Add";
+            this.Lbl_Add.Size = new System.Drawing.Size(38, 20);
+            this.Lbl_Add.TabIndex = 16;
+            this.Lbl_Add.Text = "Add";
+            this.Lbl_Add.Click += new System.EventHandler(this.Lbl_Add_Click);
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(522, 58);
+            this.richTextBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(217, 239);
+            this.richTextBox7.TabIndex = 17;
+            this.richTextBox7.Text = "";
+            // 
+            // Tb_Real1
+            // 
+            this.Tb_Real1.Location = new System.Drawing.Point(68, 83);
+            this.Tb_Real1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Real1.Name = "Tb_Real1";
+            this.Tb_Real1.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Real1.TabIndex = 18;
+            this.Tb_Real1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Tb_Real2
+            // 
+            this.Tb_Real2.Location = new System.Drawing.Point(68, 123);
+            this.Tb_Real2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Real2.Name = "Tb_Real2";
+            this.Tb_Real2.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Real2.TabIndex = 19;
+            // 
+            // Tb_Imaginary1
+            // 
+            this.Tb_Imaginary1.Location = new System.Drawing.Point(282, 88);
+            this.Tb_Imaginary1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Imaginary1.Name = "Tb_Imaginary1";
+            this.Tb_Imaginary1.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Imaginary1.TabIndex = 20;
+            this.Tb_Imaginary1.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Tb_Imaginary2
+            // 
+            this.Tb_Imaginary2.Location = new System.Drawing.Point(282, 129);
+            this.Tb_Imaginary2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Imaginary2.Name = "Tb_Imaginary2";
+            this.Tb_Imaginary2.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Imaginary2.TabIndex = 21;
+            // 
+            // Tb_Real3
+            // 
+            this.Tb_Real3.Location = new System.Drawing.Point(68, 202);
+            this.Tb_Real3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Real3.Name = "Tb_Real3";
+            this.Tb_Real3.ReadOnly = true;
+            this.Tb_Real3.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Real3.TabIndex = 22;
+            // 
+            // Tb_Imaginary3
+            // 
+            this.Tb_Imaginary3.Location = new System.Drawing.Point(282, 202);
+            this.Tb_Imaginary3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Tb_Imaginary3.Name = "Tb_Imaginary3";
+            this.Tb_Imaginary3.ReadOnly = true;
+            this.Tb_Imaginary3.Size = new System.Drawing.Size(148, 26);
+            this.Tb_Imaginary3.TabIndex = 23;
+            // 
+            // Btn_RunTests
+            // 
+            this.Btn_RunTests.Location = new System.Drawing.Point(572, 309);
+            this.Btn_RunTests.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_RunTests.Name = "Btn_RunTests";
+            this.Btn_RunTests.Size = new System.Drawing.Size(112, 35);
+            this.Btn_RunTests.TabIndex = 24;
+            this.Btn_RunTests.Text = "Run Tests";
+            this.Btn_RunTests.UseVisualStyleBackColor = true;
+            this.Btn_RunTests.Click += new System.EventHandler(this.Btn_RunTests_Click);
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.Location = new System.Drawing.Point(342, 382);
+            this.Btn_Exit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Size = new System.Drawing.Size(112, 35);
+            this.Btn_Exit.TabIndex = 25;
+            this.Btn_Exit.Text = "Exit";
+            this.Btn_Exit.UseVisualStyleBackColor = true;
+            this.Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 431);
+            this.Controls.Add(this.Btn_Exit);
+            this.Controls.Add(this.Btn_RunTests);
+            this.Controls.Add(this.Tb_Imaginary3);
+            this.Controls.Add(this.Tb_Real3);
+            this.Controls.Add(this.Tb_Imaginary2);
+            this.Controls.Add(this.Tb_Imaginary1);
+            this.Controls.Add(this.Tb_Real2);
+            this.Controls.Add(this.Tb_Real1);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.Lbl_Add);
+            this.Controls.Add(this.Lbl_i3);
+            this.Controls.Add(this.Lbl_i2);
+            this.Controls.Add(this.Lbl_i1);
+            this.Controls.Add(this.Lbl_horizontalLine);
+            this.Controls.Add(this.Lbl_plusSign3);
+            this.Controls.Add(this.Lbl_plusSign2);
+            this.Controls.Add(this.Lbl_plusSign1);
+            this.Controls.Add(this.Lbl_ImaginaryNumbers);
+            this.Controls.Add(this.Lbl_RealNumbers);
+            this.Controls.Add(this.Calculate);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Lab #3: Complex Arithmetic Calculator  -- Adam Armstrong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label Lbl_RealNumbers;
+        private System.Windows.Forms.Label Lbl_ImaginaryNumbers;
+        private System.Windows.Forms.Label Lbl_plusSign1;
+        private System.Windows.Forms.Label Lbl_plusSign2;
+        private System.Windows.Forms.Label Lbl_plusSign3;
+        private System.Windows.Forms.Label Lbl_horizontalLine;
+        private System.Windows.Forms.Label Lbl_i1;
+        private System.Windows.Forms.Label Lbl_i2;
+        private System.Windows.Forms.Label Lbl_i3;
+        private System.Windows.Forms.Label Lbl_Add;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.TextBox Tb_Real1;
+        private System.Windows.Forms.TextBox Tb_Real2;
+        private System.Windows.Forms.TextBox Tb_Imaginary1;
+        private System.Windows.Forms.TextBox Tb_Imaginary2;
+        private System.Windows.Forms.TextBox Tb_Real3;
+        private System.Windows.Forms.TextBox Tb_Imaginary3;
+        private System.Windows.Forms.Button Btn_RunTests;
+        private System.Windows.Forms.Button Btn_Exit;
+    }
+}
+
---
+++ Lab_03_AHA/Lab_03_AHA/Form1.cs
@@ -0,0 +1,191 @@
+﻿// Project Prolog
+// Name: Adam Armstrong
+// CS3260 Section 001
+// Project: Lab_03
+// Date: 09/03/2018 - 09/07/2018
+// Purpose: A complex arithmetic calculator that will add and subtract complex numbers.
+//
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes plagiarism, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_03_AHA {
+    // Form1:
+    public partial class Form1 : Form {
+
+        // Form1:
+        public Form1() {
+            InitializeComponent();
+        }
+
+        // Form1 Load:
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        // -----------------------------------------------------------------------------
+
+            // Rich TextBox 2 -- text changed:
+            private void richTextBox2_TextChanged(object sender, EventArgs e) { }
+
+            // Rich TextBox 5 -- text changed:
+            private void richTextBox5_TextChanged(object sender, EventArgs e) { }
+
+            // Rich TextBox 6 -- text changed:
+            private void richTextBox6_TextChanged(object sender, EventArgs e) { }
+
+            // Clicked on "Add" label:
+            private void Lbl_Add_Click(object sender, EventArgs e) { }
+
+            // Text Box 1 -- Text Changed:
+            private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+            // Text Box 3 -- Text Changed:
+            private void textBox3_TextChanged(object sender, EventArgs e) { }
+
+        // -----------------------------------------------------------------------------
+
+        // Clicked on "Calculate" button:
+        private void Calculate_Click(object sender, EventArgs e) {
+            
+        }
+
+        // Clicked on "Run Tests" button:
+        private void Btn_RunTests_Click(object sender, EventArgs e) {
+
+        }
+
+        // Clicked on "Exit" button:
+        private void Btn_Exit_Click(object sender, EventArgs e) {
+
+        }
+    } // END Form1
+
+
+    // Design and program a ComplexArithmetic class,
+    // which does calculations of addition, subtraction, multiplication and division
+    // on objects of the ComplexData class.
+
+
+    // Complex Data:
+    public class ComplexData {
+        //  Remember that a complex number consists of a signed real number and a signed imaginary number
+        //  i.e. (+/-) real_number + (+/-)imaginary_number i where i = √-1.
+        //  i.e. real + imagi.
+
+        double real_number { get; set; }
+        double imaginary_number { get; set; }
+
+        // ComplexData Constructor:
+        public ComplexData(double real, double imagi) {
+            real_number = real;
+            imaginary_number = imagi;
+        } // END constructor
+
+        // Operator "+" Overload for ComplexData:
+        public static ComplexData operator +(ComplexData num1, ComplexData num2) {
+            return new ComplexData(num1.real_number + num2.real_number, num2.imaginary_number + num2.imaginary_number);
+        }
+
+        // Operator "-" Overload for ComplexData:
+        public static ComplexData operator -(ComplexData num1, ComplexData num2) {
+            return new ComplexData(num1.real_number - num2.real_number, num2.imaginary_number - num2.imaginary_number);
+        }
+
+        // Operator "*" Overload for ComplexData:
+        public static ComplexData operator *(ComplexData num1, ComplexData num2) {
+            return new ComplexData(num1.real_number * num2.real_number, num2.imaginary_number * num2.imaginary_number);
+        }
+
+        // Operator "/" Overload for ComplexData:
+        public static ComplexData operator /(ComplexData num1, ComplexData num2) {
+            return new ComplexData(num1.real_number / num2.real_number, num2.imaginary_number / num2.imaginary_number);
+        }
+    } // END ComplexData
+
+
+    // Complex Arithmetic:
+    public class ComplexArithmetic {
+        ComplexData data;
+
+        // ComplexArithmetic Constructor:
+        public ComplexArithmetic(ComplexData cd) {
+            /data = cd;
+
+            ComplexData.Set
+                
+                (Tb_Real1.Text, Tb_Imaginary1.Text);
+        } // END constructor
+
+        // addition:
+        private void addition() {
+
+        }
+
+        // subtraction:
+        private void subtraction() {
+
+        }
+
+        // multiplication:
+        private void multiplication() {
+
+        }
+
+        // division:
+        private void division() {
+
+        }
+
+    } // END ComplexArithmetic
+
+} // END namespace Lab_03_AHA
+
+
+/*  Write a GUI (Windows Form Application) based C# program, which is a complex arithmetic calculator that will add and subtract complex numbers.
+    Design and program a ComplexArithmetic class, which does calculations of addition, subtraction, multiplication and division on objects of the ComplexData class.
+    The ComplexData class holds a single complex number; however, DO NOT use the classes provided in C#, write your own classes. 
+
+    Remember that a complex number consists of a signed real number and a signed imaginary number
+        i.e. (+/-) real_number + (+/-)imaginary_number i where i = √-1.
+        i.e. real + imag i.
+
+    Your complex number calculator program will add, subtract, multiply and divide two complex numbers.
+    Use properties (get/set) assessors/mutators for the real or imaginary value of the complex number object.
+     
+          operand1      - 2.0 + 3.0i   stored as     (- 2.0) +  (+3.0i)
+          operand2      +5.0 – 5.0i    stored as     (+5.0) +  (- 5.0i)              
+          result        +3.0 -  2.0i   stored as     (+3.0) +  (- 2.0i)
+
+          REMEMBER: That i x i = i^2 = √-1^2 = -1
+
+
+    Addition of two complex numbers:
+        Operand1     2.2 + 3.2i
+        Operator     + (plus)
+        Operand2     3.2 + 2.3i
+        Result       5.4 + 5.5i  (re1+re2) + (im1+im2) = sumre + sumim
+
+
+    Subtraction of two complex numbers:
+        Operand1       2.2 + 3.2i
+        Operator       – (minus)
+        Operand2       3.2 + 2.3i
+        Result         -1.0 + 0.9i  (re1-re2) + (im1-im2) = difre + difim
+
+
+    You can be as creative as you wish for the user-interface as long as it is a GUI.
+    However, you MUST insure that the user cannot enter invalid data, i.e. 1a2.0 is not a valid double; however,<space>12.30<space> is a valid double!
+
+    As part of this assignment you will need to include a UML class diagram.
+*/
